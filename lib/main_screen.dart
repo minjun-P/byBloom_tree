5cca4068a35ae66c6d4038001cddb77a58ef2832
juhwancho <49906112+juhwancho@users.noreply.github.com>
2022-01-20 22:13:26 +0900

    screenutil다시넣기

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,5 +1,6 @@
 import 'package:bybloom_tree/main_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
@@ -12,6 +13,15 @@ class MainScreen extends GetView<MainController> {
 
   @override
   Widget build(BuildContext context) {
+    ScreenUtil.init(
+      BoxConstraints(
+        maxWidth: MediaQuery.of(context).size.width,
+        maxHeight: MediaQuery.of(context).size.height),
+      designSize: Size(360,690),
+      context: context,
+      minTextAdapt: true,
+      orientation: Orientation.portrait);
+
     return Obx(()=>Scaffold(
       body: IndexedStack(
         index: controller.navigationBarIndex.value,
