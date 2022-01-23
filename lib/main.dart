b8f1bab7aa2e0a6a4b254a6690bd80cdaec00dd5
juhwancho <49906112+juhwancho@users.noreply.github.com>
2022-01-23 22:00:42 +0900

    아무튼 중간

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ import 'package:firebase_core/firebase_core.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  initializeDateFormatting().then((_)=> runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
