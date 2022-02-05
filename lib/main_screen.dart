4b38a0a7c604b419dd77e7b1206d51ee2e1112fc
minjun-P <jun7332568@gmail.com>
2022-02-05 20:48:56 +0900

    수민 디자인 30%

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -2,79 +2,62 @@ import 'package:bybloom_tree/main_controller.dart';
 import 'package:bybloom_tree/pages/forest_page/forest_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-import 'drawer.dart';
 import 'pages/forest_page/forest_page.dart';
 import 'pages/mission_page/mission_page.dart';
+import 'pages/storage_page/storage_page.dart';
 import 'pages/tree_page/tree_page.dart';
 import 'my_flutter_app_icons.dart';
 
 class MainScreen extends GetView<MainController> {
-  final GlobalKey<ScaffoldState> _scaffoldKey= GlobalKey<ScaffoldState>();
+  const MainScreen({Key? key}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
-    ScreenUtil.init(
-      BoxConstraints(
-        maxWidth: MediaQuery.of(context).size.width,
-        maxHeight: MediaQuery.of(context).size.height),
-      designSize: Size(360,690),
-      context: context,
-      minTextAdapt: true,
-      orientation: Orientation.portrait);
-
-    return Obx(()=>Scaffold(
-      key: _scaffoldKey,
-      extendBody: true,
-      endDrawer:MainDrawer(),
-      body: Stack(
-        children:
-        <Widget>[
-
-          IndexedStack(
-        index: controller.navigationBarIndex.value,
-        children: [
-          TreePage(),
-          MissionPage(),
-          ForestPage()
-        ],
-      ),
-          Positioned(
-              top:40,
-              right: 10,
-              child: IconButton(
-                icon:Icon(Icons.menu),
-                iconSize: 30,
-                onPressed: (){
-                  _scaffoldKey.currentState!.openEndDrawer();
-                },)),]
-      ),
-      bottomNavigationBar: ClipRRect(
-        borderRadius: BorderRadius.only(topLeft: Radius.circular(15),topRight: Radius.circular(15)),
-        child: SizedBox(
-          height: GetPlatform.isIOS? 100:80,
-          child: BottomNavigationBar(
-            backgroundColor: Colors.grey,
-            items: const [
-              BottomNavigationBarItem(icon: Icon(MyFlutterApp.tree), label: '',backgroundColor:Colors.lightGreen, ),
-              BottomNavigationBarItem(
-                  icon: Icon(MyFlutterApp.event_note), label: '', backgroundColor:Colors.lightGreen ),
-              BottomNavigationBarItem(icon: Icon(MdiIcons.forest),label: '', backgroundColor:Colors.lightGreen )
+    return SafeArea(
+      child: Obx(()=>Scaffold(
+        // extendBody - 화면 영역을 bottomNavigationBar와 앱바까지 확장시키는 것. - 높이 설정을 추가로 해줘야 함.
+        extendBody: true,
+        body: Padding(
+          padding: const EdgeInsets.only(bottom: 70),
+          child: IndexedStack(
+            index: controller.navigationBarIndex.value,
+            children: [
+              TreePage(),
+              MissionPage(),
+              ForestPage(),
+              StoragePage()
             ],
-            onTap: (index) {
-              controller.changeNavigationBarIndex(index);
-            },
-            selectedItemColor: Colors.black87,
-            unselectedItemColor: Colors.white,
-            currentIndex: controller.navigationBarIndex.value,
-            showSelectedLabels: false,
-            showUnselectedLabels: false,
-            iconSize: 35,
           ),
-          )
-      ),
-    ));
+        ),
+        bottomNavigationBar: ClipRRect(
+          borderRadius: BorderRadius.only(topRight: Radius.circular(15),topLeft: Radius.circular(15)),
+          child: SizedBox(
+            height: 80,
+            child: BottomNavigationBar(
+              backgroundColor: Colors.red,
+              items: const [
+                BottomNavigationBarItem(icon: Icon(MyFlutterApp.tree), label: ''),
+                BottomNavigationBarItem(
+                    icon: Icon(MyFlutterApp.event_note), label: ''),
+                BottomNavigationBarItem(icon: Icon(MdiIcons.forest),label: ''),
+                BottomNavigationBarItem(icon: Icon(MdiIcons.folder),label: '')
+              ],
+              onTap: (index) {
+                controller.changeNavigationBarIndex(index);
+              },
+              selectedItemColor: Colors.black87,
+              unselectedItemColor: Colors.grey[300],
+              currentIndex: controller.navigationBarIndex.value,
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
+              iconSize: 35,
+            ),
+          ),
+        ),
+      )),
+    );
   }
 }
---
+++ lib/pages/storage_page/storage_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class StoragePage extends StatelessWidget {
+  const StoragePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('StroagePage'),
+    );
+  }
+}
