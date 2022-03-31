ea4a0277d480f1665c0ceda26df03575c1021866
juhwancho <49906112+juhwancho@users.noreply.github.com>
2022-03-31 18:43:55 +0900

    숲
    시작중

--- lib/pages/forest_page/forest_controller.dart
+++ lib/pages/forest_page/forest_controller.dart
@@ -1,6 +1,6 @@
 import 'package:get/get.dart';
 
-/// 일단 비워놓음 - 나중에 쓸 일 있을 때 채우도록 하자.
+
 class ForestController extends GetxController {
 
 }
--- lib/pages/forest_page/forest_making_page.dart
+++ lib/pages/forest_page/forest_making_page.dart
@@ -5,12 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'pages/forest_chat_room.dart';
 
-
-/// 숲 페이지 - 카톡처럼 클릭하면 내부 채팅방 들어가짐.
-/// DB 연결할 껀덕지가 제일 많은 곳이 아닐까 싶음. 내부 채팅방은 아마 더...
-/// 지금은 임시로 forest_model 페이지에 만들어놓은 데이터를 가져다 대강 구현해놓음
-/// 그 데이터를 db에서 불러온 값으로 대체하면 될 듯 하다.
-/// 채팅 UI는 너가 더 잘 알테니 알아서 수정해도 될 듯.
 class ForestPage extends GetView<ForestController> {
   const ForestPage({Key? key}) : super(key: key);
 
@@ -19,9 +13,9 @@ class ForestPage extends GetView<ForestController> {
     Get.put(ForestController());
     return Scaffold(
       appBar: AppBar(
-        title: const Text('숲',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
+        title: const Text('숲 만들기',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
         actions: [IconButton(onPressed:(){ }, icon: Icon(
-          Icons.add_box))],
+            Icons.add_box))],
         backgroundColor: Colors.white,
         toolbarHeight: 80,
       ),
@@ -44,7 +38,7 @@ class ForestPage extends GetView<ForestController> {
                     width: 80,
                     height: 80,
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
+                        borderRadius: BorderRadius.circular(20),
                         gradient: LinearGradient(
                             begin: Alignment.topLeft,
                             end: Alignment.bottomRight,
@@ -54,9 +48,9 @@ class ForestPage extends GetView<ForestController> {
                               Color.fromRGBO(Random().nextInt(255), Random().nextInt(255), Random().nextInt(255), 1)
                             ]
                         ),
-                      boxShadow: const[
-                        BoxShadow(color: Colors.grey,offset: Offset(3,3),blurRadius: 3)
-                      ]
+                        boxShadow: const[
+                          BoxShadow(color: Colors.grey,offset: Offset(3,3),blurRadius: 3)
+                        ]
                     ),
                   ),
                   const SizedBox(width: 15,),
@@ -67,7 +61,7 @@ class ForestPage extends GetView<ForestController> {
                         const SizedBox(height: 10,),
                         Text(forestList[index].name,style: const TextStyle(fontSize: 18,fontWeight: FontWeight.w700),),
                         Text(
-                            forestList[index].latestMessage.content,
+                          forestList[index].latestMessage.content,
                           maxLines: 2,
                           overflow: TextOverflow.ellipsis,
                           style: const TextStyle(fontSize: 14),
@@ -85,8 +79,8 @@ class ForestPage extends GetView<ForestController> {
                       Container(
                         padding: const EdgeInsets.all(3),
                         decoration: BoxDecoration(
-                          color: Colors.red,
-                          borderRadius: BorderRadius.circular(10)
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(10)
                         ),
                         alignment: Alignment.center,
                         child: Text(forestList[index].unreadCount.toString(),style: const TextStyle(color: Colors.white,fontSize: 14),),
